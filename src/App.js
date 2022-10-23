479351a0ede0bcb8f441df940f0b93131cbe73ec
SadiKISLAR <sadi22kislar@gmail.com>
2022-10-23 23:38:20 +0300

    asdasd

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import AddTask from './components/add task/AddTask';
 import Button from './components/header/Button';
@@ -5,13 +6,41 @@ import Header from './components/header/Header';
 import Task from './components/tasks/Task';
 
 function App() {
+  const [close, setClose] = useState(true);
+  const [todo, setTodo] = useState(true);
+  const [date, setDate] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleClose = (e) => {
+    e.preventDefault();
+    setClose((prev) => (prev = !prev));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (todo !== "" && date !== "") {
+      setTodos([{ id: `${todo}-${Date.now()}`, todo, date }, ...todos]);
+      setTodo("");
+      setDate("");
+    }
+  };
+  const handleDelete = (id) => {
+    const delTodo = todos.filter((to) => to.id !== id);
+    setTodos([...delTodo]);
+  };
   return (
     <div className='App-header'>
       <div className="container">
         <Header/>
-        <Button/>
-        <AddTask/>
-        <Task/>
+        <Button onClick={handleClose}
+          {...close ? "close" : "show"}/>
+        <AddTask handleSubmit={handleSubmit}
+              todo={todo}
+              date={date}
+              setTodo={setTodo}
+              setDate={setDate}
+            />
+        <Task todos={todos} complateTodo={complateTodo} handleDelete={handleDelete}/>
       </div>
     </div>       
   );
--- src/components/tasks/Task.jsx
+++ src/components/tasks/Task.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 
 const Task = () => {
-  return (
-    <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
     
+  return (
+         <ul className="allTodos">
+      {todos.map((t, index) => (
+        <li className="singleTodo">
+          <div
+            key={index}
+            className={t.isComplete ? "inlist" : "inlist-line"}
+          >
+            <span className="todoText" onClick={() => complateTodo(t.id)} key={t.id}>
+              {t.todo}
+            </span>
+            <span className="todoDate">{t.date}</span>
+          </div>
+          <button className="btn-delete" onClick={() => handleDelete(t.id)}>
+            Delete
+          </button>
+        </li>
+      ))}
     </ul>
   )
 }
